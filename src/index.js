11c26e53f8f0a632a825ba35499ead0bd7bd694b
tran-h <43885150+tran-h@users.noreply.github.com>
2025-05-01 16:20:43 -0400

    add functionality to display projects/tasks and styling of tasks

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class Task {
     constructor(title, desc, dueDate, prio) {
         this.title = title;
         this.desc = desc;
-        this.dueDate = format(add(dueDate, {days: 1}), 'E MMM dd, yyyy'); //add a day because date-fns returns previous day
+        this.dueDate = format(add(dueDate, { days: 1 }), 'E MMM dd, yyyy'); //add a day because date-fns returns previous day
         this.prio = prio;
     }
 }
@@ -18,9 +18,9 @@ class Project {
 }
 
 const defaultProject = new Project("Default", [
-    new Task("Task 1", "This is an exmaple task", format(new Date(), 'E MMM dd, yyyy'), 1),
-    new Task("Task 2", "This is an exmaple task", format(new Date(), 'E MMM dd, yyyy'), 2),
-    new Task("Task 3", "This is an exmaple task", format(new Date(), 'E MMM dd, yyyy'), 3)
+    new Task("Task 1", "This is an example task", format(new Date(), 'E MMM dd, yyyy'), 1),
+    new Task("Task 2", "This is an example task", format(new Date(), 'E MMM dd, yyyy'), 2),
+    new Task("Task 3", "This is an example task", format(new Date(), 'E MMM dd, yyyy'), 3)
 ]);
 
 const projects = [defaultProject];
@@ -54,3 +54,70 @@ const taskTitle = document.querySelector("#taskTitle");
 const taskDesc = document.querySelector("#taskDesc");
 const taskDueDate = document.querySelector("#taskDueDate");
 const taskPrio = document.querySelector("#taskPrio");
+
+function displayMainContent(project) {
+    mainDivTitle.textContent = project.title;
+    tasksList.textContent = "";
+
+    for (let i in project.tasks) {
+        const div = document.createElement("div");
+        div.classList.add("task");
+        const taskTitle = document.createElement("p");
+        const taskDesc = document.createElement("p");
+        const taskDueDate = document.createElement("p");
+        const taskPrioColourIndicator = document.createElement("div");
+        taskTitle.textContent = project.tasks[i].title;
+        taskDesc.textContent = project.tasks[i].desc;
+        taskDueDate.textContent = `Due Date: ${project.tasks[i].dueDate}`;
+        taskPrioColourIndicator.style.width = "0.5rem"
+
+        switch (project.tasks[i].prio) {
+            case 1:
+                taskPrioColourIndicator.style.backgroundColor = "green";
+                break;
+            case 2:
+                taskPrioColourIndicator.style.backgroundColor = "yellow";
+                break;
+            case 3:
+                taskPrioColourIndicator.style.backgroundColor = "red";
+                break;
+            default:
+                break;
+        }
+
+        div.onclick = function () {
+            div.contains(taskDesc) ? taskDesc.remove() : div.append(taskDesc);
+        };
+
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        editBtn.onclick = function () {
+            //TODO: reuse task modal to edit fields
+            console.log("task edit btn clicked");
+        }
+
+        div.append(taskPrioColourIndicator);
+        div.append(taskTitle);
+        div.append(taskDueDate);
+        div.append(editBtn);
+        tasksList.append(div);
+    }
+}
+
+function displayProjects() {
+    projectsDiv.innerHTML = "";
+    for (let p in projects) {
+        const newProj = document.createElement("button");
+        newProj.id = projects[p].title;
+        newProj.textContent = projects[p].title;
+        newProj.classList.add("project");
+        newProj.addEventListener("click", function () {
+            displayMainContent(projects[p]);
+            currentProject = projects[p];
+        });
+        projectsDiv.append(newProj);
+    }
+}
+
+displayProjects();
+displayMainContent(defaultProject);
